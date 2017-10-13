f0368f817d7b3dda0c9a610f7af58d16e4b6afcd
CristyDry <519461154@qq.com>
2017-10-13 10:32:26 +0800

    111

--- src/main/java/com/fullteem/common/utils/Log.java
+++ src/main/java/com/fullteem/common/utils/Log.java
@@ -4,5 +4,6 @@ package com.fullteem.common.utils;
  * Created by LeWis on 2017/10/13.
  */
 public class Log {
-    public static void println(String log){}
+    public static void println(Object log){}
+    public static void print(Object log){}
 }
